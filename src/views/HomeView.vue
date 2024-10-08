840f7b3455a4a7560da34729357fe1413ea8e97d
dj <dj.atlassian@cura-go.de>
2024-10-08 21:27:57 +0200

    base

---
+++ src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="main">
+    <h1>This is the main page</h1>
+  </div>
+</template>
+
+<style scoped>
+@media (min-width: 1024px) {
+  .main {
+    background-color: #0000ff;
+    min-height: 70vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
